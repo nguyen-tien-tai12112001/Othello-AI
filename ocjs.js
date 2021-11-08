c3b359dfcdd6fe26fc0444b67fd8a91f8b64d91a
nguyen tien tai <anhtaibnvn12112001@gmail.com>
2021-11-08 22:40:56 +0700

    dich xong file ocjs.js

--- ocjs.js
+++ ocjs.js
@@ -9,8 +9,8 @@ function Chessboard() {
 
   _this.toDown = null; //Nằm xuống
 
+  //Ràng buộc sự kiện nhấp chuột
   function bindEvent(td) {
-    //Ràng buộc sự kiện nhấp chuột
     for (var i = 0; i < 64; i++)
       (function (i) {
         td[i].onclick = function () {
@@ -22,10 +22,10 @@ function Chessboard() {
     td = undefined;
   }
 
+  //Tạo bàn cờ
   _this.create = function () {
-    //创建棋盘
     var obj = document.getElementById("chessboard");
-    var html = "<table>"; //这个地方创建是通过改变html来创建的，使用table创建
+    var html = "<table>";
     for (var i = 0; i < 8; i++) {
       html += "<tr>";
       for (var j = 0; j < 8; j++)
@@ -44,10 +44,8 @@ function Chessboard() {
       "-1": document.getElementById("side2"),
     };
   };
-
+  //Cập nhật bảng
   _this.update = function (m, nop) {
-    //更新棋盘
-    //给入map
     for (var i = 0; i < 64; i++)
       pieces[i].className = ["white", "", "black"][m[i] + 1];
     if (!nop) for (var n in m.next) pieces[n].className = "prompt";
@@ -61,134 +59,133 @@ function Chessboard() {
   };
 }
 
-/*棋盘逻辑类*/
+//Bàn cờ logic
 function Othello() {
-  var _this = this; //_this申明是类本身
+  var _this = this;
 
-  var map = []; //棋局数组
-  var history = []; //历史记录,用于悔棋操作
+  var map = [];
+  var history = [];
 
   var zobrist = new Zobrist();
   _this.aiSide = 0; //先行方：1: 电脑为黑棋,  -1: 电脑为白棋,  0: 双人对战 2: 电脑自己对战
 
-  var aiRuning = false; //AI运算中...
-  var aiRuningObj = document.getElementById("airuning"); //也就是指示出现提示框
-  var passObj = document.getElementById("pass"); //没有棋下的时候就返回这个
+  var aiRuning = false; //AI.....
+  var aiRuningObj = document.getElementById("airuning"); //Đó là để chỉ ra rằng một hộp nhắc xuất hiện
+  var passObj = document.getElementById("pass"); //Trả lại cái này khi không có cờ
 
-  var timer; //定时器id：局时
+  var timer; //Id hẹn giờ: thời gian trò chơi
 
-  var aiNum = 1; //ai的个数
+  var aiNum = 1; //số lượng ai
 
   _this.play = function () {
-    //开始新棋局：所有的数据初始化都在这里，这个是main
+    //Bắt đầu một trò chơi mới: tất cả dữ liệu khởi tạo đều ở đây, đây là chính
 
     if (aiRuning)
-      //要是ai运行就跳过
+      //Bỏ qua nếu ai đang chạy
       return;
-    clearTimeout(timer); //清空计时器
+    clearTimeout(timer); //Xóa bộ hẹn giờ
     console.clear();
-    //console.time("计时器1");
 
-    //棋盘初始化
+    //Khởi tạo bảng
     map = [];
     for (var i = 0; i < 64; i++) map[i] = 0; //空格为 0
-    map[28] = map[35] = 1; //黑子为 1
-    map[27] = map[36] = -1; //白子为 -1
+    map[28] = map[35] = 1; //Đen 1
+    map[27] = map[36] = -1; //Trắng -1
 
-    map.black = map.white = 2; //黑白棋子数目
-    map.space = 60; //空格数目（64个格子，但是一开始4个是有东西的了）
+    map.black = map.white = 2; //Số lượng tốt đen và trắng
+    map.space = 60; //Số lượng khoảng trống (64 ô, nhưng ở đầu có 4 ô)
 
     map.frontier = [];
-    var tk = [18, 19, 20, 21, 26, 29, 34, 37, 42, 43, 44, 45]; //用于初始化的暂存数据
+    var tk = [18, 19, 20, 21, 26, 29, 34, 37, 42, 43, 44, 45]; //Dữ liệu tạm thời để khởi tạo
     for (var i = 0; i < 12; i++) map.frontier[tk[i]] = true;
 
-    map.side = 1; //当前执棋方（1.黑棋 0.白棋）
-    map.newPos = -1; //最新下子的位置
-    map.newRev = []; //最新反转棋子的位置
-    map.nextIndex = []; //下一步可走棋的位置
-    map.next = {}; //下一步可走棋的反转棋子
-    map.nextNum = 0; //下一步可走棋的数目
-    map.prevNum = 0; //上一步可走棋的数目
-    map.key = [0, 0]; //用于置换表的键值
+    map.side = 1; //Kỳ thủ hiện tại (1. Cờ đen 0. Cờ trắng)
+    map.newPos = -1; //Vị trí gần đây nhất
+    map.newRev = []; //Vị trí của mảnh đảo chiều mới nhất
+    map.nextIndex = []; //Chuyển động tiếp theo ở đâu
+    map.next = {}; //Các mảnh đảo ngược có thể di chuyển trong bước tiếp theo
+    map.nextNum = 0; //Số lượng nước đi có sẵn cho nước đi tiếp theo
+    map.prevNum = 0; //Số lần di chuyển có thể thực hiện trong nước đi trước đó
+    map.key = [0, 0]; //Giá trị khóa được sử dụng để thay thế bảng
 
-    history = []; //历史记录
+    history = []; //kỷ lục lịch sử
 
-    update(); //update更新上面的初始化数据
+    update(); //cập nhật cập nhật dữ liệu khởi tạo ở trên
   };
 
   function update() {
-    //每次更新棋盘：判断是否可走，
-    var aiAuto = _this.aiSide == map.side || _this.aiSide == 2; //这个意思是，aiAuto=后面过程（ai方=map当前持方时；或者ai方==2，也就是电脑自己对战时，然后变成true)
+    //Mỗi khi bảng được cập nhật: hãy đánh giá xem nó có thể hoạt động hay không,
+    var aiAuto = _this.aiSide == map.side || _this.aiSide == 2;
+    //Điều này có nghĩa là aiAuto = quá trình sau (bên ai = bên giữ bản đồ hiện tại; hoặc bên ai == 2, nghĩa là khi máy tính đang đấu với chính nó, thì nó trở thành sự thật)
     _this.findLocation(map);
-    setAIRunStatus(false); //不显示ai在计算
-    setPassStatus(false); //不显示pass
-    board.update(map, aiAuto); //ai下棋：传入map，还有aiAuto函数
+    setAIRunStatus(false); //Đừng thể hiện rằng ai đang tính toán
+    setPassStatus(false);
+    board.update(map, aiAuto); //ai chơi cờ vua: vượt qua trong bản đồ và chức năng aiAuto
     // console.log(map.nextIndex)
 
     if (map.space == 0 || (map.nextNum == 0 && map.prevNum == 0)) {
-      //棋盘子满 或 双方都无棋可走
+      //Bàn cờ đầy hoặc không người chơi nào di chuyển được
       timer = setTimeout(gameOver, 450);
       return;
     }
     if (map.nextNum == 0) {
-      //无棋可走pass
+      //Không di chuyển để vượt qua
       timer = setTimeout(function () {
         _this.pass(map);
         update();
-        setPassStatus(true); //不显示pass
+        setPassStatus(true);
       }, 450);
       return;
     }
 
     if (aiAuto) {
-      //也就是当aiAuto是真的时候开始执行
-      aiRuning = true; //这个是打一个条幅
+      //Đó là, nó bắt đầu thực thi khi aiAuto là true
+      aiRuning = true;
       timer = setTimeout(function () {
-        setAIRunStatus(true); //AI开始运行
-        timer = setTimeout(aiRun, 50); //这个地方就是ai走棋了
+        setAIRunStatus(true); //AI bắt đầu chạy
+        timer = setTimeout(aiRun, 50); //Nơi này là ai di chuyển
       }, 400);
     }
   }
 
   function aiRun() {
-    //电脑走棋
+    //Máy tính di chuyển
     if (map.nextNum == 1)
-      //就一步棋可走了,还搜索什么?
+      //Bạn có thể đi chỉ với một lần di chuyển, còn điều gì khác để tìm kiếm?
       _this.go(map.nextIndex[0]);
     else if (map.space <= 58)
       if (_this.aiNum == 1) {
-        //这个是两步以后就开始使用startSearch来走棋了
-        //对AI进行设定
+        //Điều này là để bắt đầu sử dụng startSearch để di chuyển cờ vua sau hai nước đi
+
+        //Thiết lập AI
         _this.go(ai6.startSearch(map));
       } else {
         _this.go(ai6.startSearch(map));
       }
-    //前面两步棋都是随机走的
+    //Hai lần di chuyển đầu tiên được thực hiện ngẫu nhiên
     else _this.go(map.nextIndex[(Math.random() * map.nextIndex.length) >> 0]);
   }
   // document.getElementById("ai").onclick = aiRun;
 
   function gameOver() {
-    //终局的时候
-    // console.timeEnd("计时器1");
-    setAIRunStatus(false); //不显示ai在计算
-    setPassStatus(false); //不显示pass
+    setAIRunStatus(false); //Đừng thể hiện rằng ai đang tính toán
+    setPassStatus(false); //Không hiển thị vượt qua
     alert(
-      "棋局结束\n\n黑棋: " +
+      "Kết thúc trò chơi\n\nĐen: " +
         map.black +
-        " 子\n白棋: " +
+        " \nTrắng: " +
         map.white +
-        " 子\n\n" +
+        " \n\n" +
         (map.black == map.white
           ? "平局!!!"
           : map.black > map.white
-          ? "黑棋胜利!!!"
-          : "白棋胜利!!!")
+          ? "Chiến thắng cờ đen!!!"
+          : "Chiến thắng cờ trắng!!!")
     );
   }
 
   _this.dire = (function () {
-    //获取某一棋盘格某一方向的格子.超过边界返回64
+    //Lấy lưới theo một hướng nhất định của một bàn cờ nhất định. Nếu nó vượt quá ranh giới, hãy trả về 64
     var dr = [-8, -7, 1, 9, 8, 7, -1, -9];
     var bk = [8, 0, 0, 0, 8, 7, 7, 7];
     return function (i, d) {
@@ -198,7 +195,7 @@ function Othello() {
   })();
 
   _this.findLocation = function (m) {
-    //查找可走棋的位置
+    //Tìm nơi để di chuyển
     function is(i, j) {
       var lk = 0;
       while ((i = _this.dire(i, j)) != 64 && m[i] == -m.side) {
@@ -210,7 +207,7 @@ function Othello() {
     m.nextIndex = [];
     m.next = [];
 
-    //对AI进行设定
+    //Thiết lập AI
     if (_this.aiNum == 1) {
       var hist = ai6.history[m.side == 1 ? 0 : 1][m.space];
     } else {
@@ -229,27 +226,28 @@ function Othello() {
         m.nextIndex.push(fi); //
       }
     }
-    m.nextNum = m.nextIndex.length; //这个是为了pass使用
+    m.nextNum = m.nextIndex.length; //Đây là để sử dụng thông qua
   };
 
   _this.pass = function (m) {
-    //一方无棋可走就pass
-    m.side = -m.side; //下棋方
-    m.prevNum = m.nextNum; //历史记录次序往后一个
-    zobrist.swap(m.key); //调用zobrist函数里面的交换持方的方法
+    //Nếu một bên không có cờ để đi, hãy vượt qua
+    m.side = -m.side; //Người chơi cờ
+    m.prevNum = m.nextNum; //Thứ tự của các bản ghi lịch sử là một tiếp theo
+    zobrist.swap(m.key); //Gọi phương thức chủ sở hữu trao đổi trong hàm zobrist
   };
 
   _this.newMap = function (m, n) {
-    //返回新的棋局
-    //m给入map，n给入下一步棋的位置
+    //Quay lại một trò chơi mới
+    //m là bản đồ, n là vị trí của nước đi tiếp theo
 
     var nm = m.slice(0);
     nm[n] = m.side;
 
-    nm.key = m.key.slice(0); //复制数组
+    nm.key = m.key.slice(0);
+    //Sao chép mảng
     zobrist.set(nm.key, m.side == 1 ? 0 : 1, n);
 
-    nm.frontier = m.frontier.slice(0); //复制数组
+    nm.frontier = m.frontier.slice(0);
     nm.frontier[n] = false;
     for (var i = 0; i < 8; i++) {
       var k = _this.dire(n, i);
@@ -259,11 +257,11 @@ function Othello() {
     var ne = m.next[n];
     var l = ne.length;
     for (var i = 0; i < l; i++) {
-      nm[ne[i]] = m.side; //反转的棋子
+      nm[ne[i]] = m.side; //Cầm đồ ngược
       zobrist.set(nm.key, 2, ne[i]);
     }
 
-    //下面计算空格数、黑棋数、白棋数
+    //Sau đây tính toán số lượng khoảng trắng, số lượng nước đi màu đen và số lượng nước đi màu trắng
     if (m.side == 1) {
       nm.black = m.black + l + 1;
       nm.white = m.white - l;
@@ -272,22 +270,22 @@ function Othello() {
       nm.black = m.black - l;
     }
 
-    nm.space = 64 - nm.black - nm.white; //空格数目
+    nm.space = 64 - nm.black - nm.white; //Số lượng khoảng trắng
     nm.side = -m.side;
     nm.prevNum = m.nextNum;
 
-    zobrist.swap(nm.key); //交换持方
+    zobrist.swap(nm.key); //Chủ sở hữu trao đổi
     return nm;
   };
 
   _this.goChess = function (n) {
-    //走棋
+    //Di chuyển
     history.push(map);
     _this.go(n);
   };
 
   _this.go = function (n) {
-    //走棋
+    //Di chuyển
 
     aiRuning = false;
 
@@ -296,12 +294,11 @@ function Othello() {
     map = _this.newMap(map, n);
     map.newRev = rev;
     map.newPos = n;
-    // console.log(map.key);
+
     update();
   };
 
   _this.historyBack = function () {
-    //悔棋功能
     if (aiRuning || history.length == 0) return;
     clearTimeout(timer);
     map = history.pop();
@@ -309,18 +306,18 @@ function Othello() {
   };
 
   function setAIRunStatus(t) {
-    //设置AI运算状态
+    //Đặt trạng thái hoạt động của AI
     aiRuningObj.style.display = t ? "block" : "none";
   }
 
   function setPassStatus(t) {
-    //设置pass状态：无棋可下就pass
+    //Đặt trạng thái vượt qua: vượt qua nếu không có cờ để chơi
     passObj.style.display = t ? "block" : "none";
     if (t)
       passObj.innerHTML =
         map.side == 1
-          ? "白方无棋可下，黑方继续下子"
-          : "黑方无棋可下，白方继续下子";
+          ? "Trắng không có nước đi để chơi, và Đen tiếp tục chơi"
+          : "Đen không có nước đi để chơi, và Trắng tiếp tục chơi";
   }
 }
 
@@ -337,16 +334,16 @@ function Zobrist() {
     zarr[2][pn] = [
       zarr[0][pn][0] ^ zarr[1][pn][0],
       zarr[0][pn][1] ^ zarr[1][pn][1],
-    ]; // 各位置上翻棋时
+    ]; // Khi lật từng vị trí
   }
 
   function rnd() {
-    //获取32位的随机数
+    //Nhận một số ngẫu nhiên 32 bit
     return (Math.random() * 0x100000000) >> 0;
   }
 
   _this.swap = function (key) {
-    //执棋方轮换
+    //Xoay người chơi
     key[0] ^= swapSide[0];
     key[1] ^= swapSide[1];
   };
@@ -366,15 +363,14 @@ board.create();
 board.toDown = othe.goChess;
 
 document.getElementById("play").onclick = function () {
-  //开始+重新开始
   document.getElementById("selectbox").style.display = "block";
 };
 document.getElementById("ok").onclick = function () {
-  //选择难度，先后手以后，点击确定以后。
+  //Sau khi chọn mức độ khó, bấm OK sau khi bạn đã chơi theo trình tự.
   document.getElementById("selectbox").style.display = "none";
   var ro = document.getElementById("selectbox").getElementsByTagName("input");
 
-  othe.aiSide = ro[0].checked ? -1 : 1; //先走方
+  othe.aiSide = ro[0].checked ? -1 : 1; //Đi đầu tiên
 
   for (var i = 2; i < ro.length; i++) if (ro[i].checked) break;
   othe.aiNum = i - 1;
@@ -414,13 +410,12 @@ document.getElementById("cancel").onclick = function () {
 };
 
 document.getElementById("back").onclick = function () {
-  //悔棋
   othe.historyBack();
 };
 
-document.getElementById("no3d").onclick = function () {
-  //3D棋盘切换
-  var desk = document.getElementById("desk");
-  desk.className = desk.className == "fdd" ? "" : "fdd";
-  this.innerHTML = desk.className == "fdd" ? "2D" : "3D";
-};
+// document.getElementById("no3d").onclick = function () {
+//   //3D棋盘切换
+//   var desk = document.getElementById("desk");
+//   desk.className = desk.className == "fdd" ? "" : "fdd";
+//   this.innerHTML = desk.className == "fdd" ? "2D" : "3D";
+// };
