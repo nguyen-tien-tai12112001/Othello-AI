98afea2bbb028eab7b2f13e4f7108478056c8cba
nguyen tien tai <anhtaibnvn12112001@gmail.com>
2021-11-08 22:46:57 +0700

    dich xong file AI

--- AI6.js
+++ AI6.js
@@ -1,291 +1,285 @@
- "use strict";
- 
- 
-/*AI类*/
-function AI6(){
-	var oo = this;
+"use strict";
 
-	oo.calculateTime = 1000;	//限制每步棋计算的时间
-	oo.outcomeDepth = 14;		//终局搜索深度
-	var outcomeCoarse = 15;		//终局搜索模糊模式搜索深度
-	var maxDepth;
-	var outTime;
-	
-	var weight = [6,11,2,2,3]; 
+/*AI*/
+function AI6() {
+  var _this = this;
 
-	var rnd = [
-		{s: 0,a: 1,b: 8,c: 9,dr:[1,8]},
-		{s: 7,a: 6,b:15,c:14,dr:[-1,8]},
-		{s:56,a:57,b:48,c:49,dr:[1,-8]},
-		{s:63,a:62,b:55,c:54,dr:[-1,-8]}
-	];
+  _this.calculateTime = 1000; //Giới hạn thời gian tính toán của mỗi lần di chuyển
+  _this.outcomeDepth = 14; //Độ sâu tìm kiếm cuối cùng
+  var outcomeCoarse = 15; //Độ sâu tìm kiếm mẫu tìm kiếm cuối cùng
+  var maxDepth;
+  var outTime;
 
-	oo.history = [[],[]];			//历史启发表
-	for (var i=0; i<2; i++)
-		for (var j=0; j<=60; j++)
-			oo.history[i][j] = [0,63,7,56,37,26,20,43,19,29,34,44,21,42,45,18,2,61,23,40,5,58,47,16,10,53,22,41,13,46,17,50,51,52,12,11,30,38,25,33,4,3,59,60,39,31,24,32,1,62,15,48,8,55,6,57,9,54,14,49];
-		
-		
-		
-		
-	var hash = new Transposition();//置换表类
+  var weight = [6, 11, 2, 2, 3];
 
-	
-	
-	
-	function sgn(n){//符号函数
-		return n>0?1:n<0?-1:0;
-	}
+  var rnd = [
+    { s: 0, a: 1, b: 8, c: 9, dr: [1, 8] },
+    { s: 7, a: 6, b: 15, c: 14, dr: [-1, 8] },
+    { s: 56, a: 57, b: 48, c: 49, dr: [1, -8] },
+    { s: 63, a: 62, b: 55, c: 54, dr: [-1, -8] },
+  ];
 
-	oo.startSearch = function(m){//main：开始搜索博弈树：最大最小搜索（估价函数）+alphabeta剪枝+mtd算法调用
-	
-		// hash = new Transposition();
-		// console.profile('性能分析器一');
-		var f = 0;
-		if (m.space<=oo.outcomeDepth){//小于深度就开始搜索(也就是最后的结局阶段了)
-			//进行终局搜索
-			outTime = (new Date()).getTime()+600000;//终局搜索就不限时间了
-			maxDepth = m.space;
-			//console.time("计时器2");
-			if (maxDepth>=outcomeCoarse)
-				f = alphaBeta(m, maxDepth, -Infinity, Infinity);//alpha-beta剪枝：α=-∞；β=+∞
-			else
-				f = mtd(m, maxDepth, f);//mtd(f)
-			//console.timeEnd("计时器2");
-			console.log("终局搜索结果：",maxDepth,m.space,m.side,f*m.side);
-			return hash.getBest(m.key);//置换表的getBest方法：
-		}
+  _this.history = [[], []]; //Bảng khai sáng lịch sử
+  for (var i = 0; i < 2; i++)
+    for (var j = 0; j <= 60; j++)
+      _this.history[i][j] = [
+        0, 63, 7, 56, 37, 26, 20, 43, 19, 29, 34, 44, 21, 42, 45, 18, 2, 61, 23,
+        40, 5, 58, 47, 16, 10, 53, 22, 41, 13, 46, 17, 50, 51, 52, 12, 11, 30,
+        38, 25, 33, 4, 3, 59, 60, 39, 31, 24, 32, 1, 62, 15, 48, 8, 55, 6, 57,
+        9, 54, 14, 49,
+      ];
 
-		outTime = (new Date()).getTime()+oo.calculateTime;
-		maxDepth = 0;
-		//console.time("计时器2");
-		try {
-			while (maxDepth<m.space){
-				f = mtd(m, ++maxDepth, f);//mtd(f)
-				// f = alphaBeta(m, ++maxDepth, -Infinity, Infinity);
-				var best = hash.getBest(m.key);
-				console.log(maxDepth,f*m.side,best);
-			}
-		} catch(eo){
-			if (eo.message!="time out")		
-				throw eo;	
-		}
-		//console.timeEnd("计时器2");
-		// console.profileEnd();
-		console.log("搜索结果：",maxDepth-1,m.space,m.side,f*m.side);
-		return best;//得到的是最好的步数坐标？
-	}
+  var hash = new Transposition(); //Lớp bảng hoán vị
 
+  function sgn(n) {
+    //Chức năng tượng trưng
+    return n > 0 ? 1 : n < 0 ? -1 : 0;
+  }
 
-	
-	
-	function evaluation(m){//估价函数
-		var corner = 0, steady = 0, uk = {};//corner占角的可能性；steady稳定子，uk
-		for (var i=0,v,l = rnd.length; v = rnd[i],i<l; i++){
-			if (m[v.s]==0){		
-				corner += m[v.a] * -3;		//次要危险点
-				corner += m[v.b] * -3;		//次要危险点
-				corner += m[v.c] * -6;		//主要危险点
-				continue;
-			}
-			corner += m[v.s] * 15;		//角点
-			steady += m[v.s];		//角也是稳定子
-			
-			for (var k = 0; k <2; k++){
-				if (uk[v.s+v.dr[k]])
-					continue;
-				var eb = true, tmp = 0;
-				for (var j = 1; j <= 6; j++)
-				{
-					var t = m[v.s+v.dr[k]*j];
-					if (t==0)
-							break;
-					else if (eb && t==m[v.s])
-						steady += t;		//稳定子
-					else
-					{
-						eb = false;
-						tmp += t;		//稳定子
-					}
-				}
-				if (j==7 && m[v.s+v.dr[k]*7]!=0)
-				{
-					steady += tmp;
-					uk[v.s+v.dr[k]*6] = true;
-				}
-			}
-		}
+  _this.startSearch = function (m) {
+    //main：Bắt đầu tìm kiếm cây trò chơi: tìm kiếm tối đa và tối thiểu (chức năng định giá）
+    //+alphabeta cắt tỉa+mtd Lời gọi thuật toán
 
-		var frontier = 0;		//前沿子
-		for (var i=9; i<=54; i+=(i&7)==6?3:1){
-			if (m[i]==0)
-				continue;
-			for (var j=0; j<8; j++)
-				if (m[othe.dire(i,j)]==0)
-				{
-					frontier -= m[i];
-					break;
-				}
-		}
+    // hash = new Transposition();
+    // console.profile('Trình phân tích hiệu suất một');
+    var f = 0;
+    if (m.space <= _this.outcomeDepth) {
+      //Bắt đầu tìm kiếm khi nó nhỏ hơn độ sâu (nghĩa là giai đoạn kết thúc cuối cùng)
+      //Thực hiện tìm kiếm cuối cùng
+      outTime = new Date().getTime() + 600000; //Không giới hạn thời gian cho lần tìm kiếm cuối cùng
+      maxDepth = m.space;
+      //console.time("Hẹn giờ2");
+      if (maxDepth >= outcomeCoarse)
+        f = alphaBeta(m, maxDepth, -Infinity, Infinity);
+      //alpha-betaCắt tỉa：α=-∞；β=+∞
+      else f = mtd(m, maxDepth, f); //mtd(f)
+      //console.timeEnd("Hẹn giờ2");
+      console.log("终局搜索结果：", maxDepth, m.space, m.side, f * m.side); //Kết quả tìm kiếm cuối cùng
+      return hash.getBest(m.key); //Phương thức getBest của bảng thay thế：
+    }
 
-		var mobility = (m.nextNum-m.prevNum)*m.side;	//行动力
+    outTime = new Date().getTime() + _this.calculateTime;
+    maxDepth = 0;
+    //console.time("hẹn giờ2");
+    try {
+      while (maxDepth < m.space) {
+        f = mtd(m, ++maxDepth, f); //mtd(f)
+        // f = alphaBeta(m, ++maxDepth, -Infinity, Infinity);
+        var best = hash.getBest(m.key);
+        console.log(maxDepth, f * m.side, best);
+      }
+    } catch (eo) {
+      if (eo.message != "time out") throw eo;
+    }
+    //console.timeEnd("hẹn giờ 2");
+    // console.profileEnd();
+    console.log("搜索结果：", maxDepth - 1, m.space, m.side, f * m.side); //kết quả tìm kiếm
+    return best; //Tọa độ bước tốt nhất là gì?
+  };
 
-		var parity = m.space<18 ? (m.space%2==0?-m.side:m.side) : 0;	//奇偶性
+  function evaluation(m) {
+    //Chức năng định giá
+    var corner = 0,
+      steady = 0,
+      uk = {}; //Chức năng định giá，uk
+    for (var i = 0, v, l = rnd.length; (v = rnd[i]), i < l; i++) {
+      if (m[v.s] == 0) {
+        corner += m[v.a] * -3; //Nguy hiểm thứ cấp
+        corner += m[v.b] * -3; //Nguy hiểm thứ cấp
+        corner += m[v.c] * -6; //Nguy hiểm thứ cấp
+        continue;
+      }
+      corner += m[v.s] * 15; //góc
+      steady += m[v.s]; //Còi cũng ổn định
 
-		var rv = corner*weight[0] + steady*weight[1] + frontier*weight[2] + mobility*weight[3] + parity*weight[4];
-		return rv * m.side;
-	}
-	
-	
-	function outcome(m){//终局结果
-		var s = m.black-m.white;
-		if (maxDepth>=outcomeCoarse)
-			return sgn(s)*10000*m.side;
-		return (s+m.space*sgn(s))*10000*m.side;
-	}
+      for (var k = 0; k < 2; k++) {
+        if (uk[v.s + v.dr[k]]) continue;
+        var eb = true,
+          tmp = 0;
+        for (var j = 1; j <= 6; j++) {
+          var t = m[v.s + v.dr[k] * j];
+          if (t == 0) break;
+          else if (eb && t == m[v.s]) steady += t;
+          //Chất ổn định
+          else {
+            eb = false;
+            tmp += t; //Chất ổn định
+          }
+        }
+        if (j == 7 && m[v.s + v.dr[k] * 7] != 0) {
+          steady += tmp;
+          uk[v.s + v.dr[k] * 6] = true;
+        }
+      }
+    }
 
-	
-	function mtd(m,depth,f){//MTD(f)算法
-		//这个算法是对alphabeta剪枝的优化
-		var lower = -Infinity;
-		var upper = Infinity;
-		do {
-			var beta = (f==lower) ? f+1 : f;	// 确定试探值
-			f = alphaBeta(m, depth, beta-1, beta);	// 进行零宽窗口试探
-			if (f < beta)
-				upper = f;
-			else
-				lower = f;
-		} while (lower < upper);
-		if (f < beta)	// 如果最后一次搜索得到的只是上限，需再搜索一次，确保获得正确的最佳棋步
-			f = alphaBeta(m, depth, f-1, f);
-		return f;
-	}
+    var frontier = 0; //Biên giới
+    for (var i = 9; i <= 54; i += (i & 7) == 6 ? 3 : 1) {
+      if (m[i] == 0) continue;
+      for (var j = 0; j < 8; j++)
+        if (m[othe.dire(i, j)] == 0) {
+          frontier -= m[i];
+          break;
+        }
+    }
 
-	function alphaBeta(m,depth,alpha,beta){//Alpha-beta剪枝
-	
-		if ((new Date()).getTime() > outTime)		
-			throw new Error("time out");	
+    var mobility = (m.nextNum - m.prevNum) * m.side; //Lực lượng hành động
 
-		var hv = hash.get(m.key,depth,alpha,beta);//置换表的get方法
-		if (hv !== false)
-			return hv;
+    var parity = m.space < 18 ? (m.space % 2 == 0 ? -m.side : m.side) : 0; //Ngang bằng
 
-		if (m.space==0)			//棋盘子满
-			return outcome(m);	//直接返回终局结果
-		
-		othe.findLocation(m);
-		
-		if (m.nextNum==0){		//判断无棋可走
-			if (m.prevNum==0)		//判断上一步也是无棋可走
-				return outcome(m);		//直接返回终局结果
-			othe.pass(m);//执行跳过
-			return -alphaBeta(m, depth, -beta, -alpha);
-		}
-		
-		if (depth<=0){			//搜索深度到达设置的极限
-			var e = evaluation(m);
-			hash.set(m.key,e,depth,0,null);//置换表的set方法
-			return e;//到达最深的时候就返回评估值
-		}
+    var rv =
+      corner * weight[0] +
+      steady * weight[1] +
+      frontier * weight[2] +
+      mobility * weight[3] +
+      parity * weight[4];
+    return rv * m.side;
+  }
 
-		var hd = hash.getBest(m.key);//给出的是phashe.best
-		if (hd!==null)
-			moveToHead(m.nextIndex,hd);//到顶层节点
+  function outcome(m) {
+    //Kết quả cuối cùng
+    var s = m.black - m.white;
+    if (maxDepth >= outcomeCoarse) return sgn(s) * 10000 * m.side;
+    return (s + m.space * sgn(s)) * 10000 * m.side;
+  }
 
-		
-		var hist = oo.history[m.side==1?0:1][m.space];//历史启发表赋值给history
-		var hashf = 1;				//最佳估值类型, 0为精确值, 1为<=alpha, 2为>=beta
-		var bestVal = -Infinity;		//记录最佳估值
-		var bestAct = null;				//记录最佳棋步
-		for (var i=0; i<m.nextNum; i++){//不断对下一层进行剪枝搜索
-			var n = m.nextIndex[i];
-			var v = -alphaBeta(othe.newMap(m,n), depth-1, -beta, -alpha);
-			if (v > bestVal){
-				bestVal = v;
-				bestAct = n;
-				if (v > alpha){
-					alpha = v;
-					hashf = 0;//给出精确值
-					moveToUp(hist,n);//回到上层节点
-				}
-				if (v >= beta){
-					hashf = 2;
-					break;		//发生剪枝
-				}
-			}
-		}
-		moveToHead(hist,bestAct);//回到顶层节点
-		hash.set(m.key,bestVal,depth,hashf,bestAct);//置换表的set方法：bestAct从这里返回的
-		return bestVal;//这个感觉和上面的evaluation的最后值是一样的
-	}
+  function mtd(m, depth, f) {
+    //MTD(f)thuật toán
+    //Thuật toán này là sự tối ưu hóa của việc cắt tỉa alpha
+    var lower = -Infinity;
+    var upper = Infinity;
+    do {
+      var beta = f == lower ? f + 1 : f; // Xác định giá trị heuristic
+      f = alphaBeta(m, depth, beta - 1, beta); // Kiểm tra cửa sổ độ rộng bằng không
+      if (f < beta) upper = f;
+      else lower = f;
+    } while (lower < upper);
+    if (f < beta)
+      // Nếu lần tìm kiếm cuối cùng chỉ là giới hạn trên,
+      //bạn cần phải tìm kiếm lại để đảm bảo rằng bạn nhận được chính xác nước đi tốt nhất
+      f = alphaBeta(m, depth, f - 1, f);
+    return f;
+  }
 
-	
-	
-	function moveToHead(arr,n){//alphabeta里面用于返回树的顶层
-		if (arr[0]==n)
-			return;
-		var i = arr.indexOf(n);
-		arr.splice(i,1);
-		arr.unshift(n);
-	}
+  function alphaBeta(m, depth, alpha, beta) {
+    //Alpha-beta Cắt tỉa
 
-	function moveToUp(arr,n){//alphabeta里面用于返回上层节点
-		if (arr[0]==n)
-			return;
-		var i = arr.indexOf(n);
-		arr[i] = arr[i-1];
-		arr[i-1] = n;
-	}
+    if (new Date().getTime() > outTime) throw new Error("time out");
 
+    var hv = hash.get(m.key, depth, alpha, beta); //Phương thức get của bảng thay thế
+    if (hv !== false) return hv;
+
+    if (m.space == 0)
+      //Bàn cờ đầy đủ
+      return outcome(m); //Quay lại trực tiếp kết quả cuối cùng
+
+    othe.findLocation(m);
+
+    if (m.nextNum == 0) {
+      //Đánh giá rằng không có động thái
+      if (m.prevNum == 0)
+        //Phán đoán không có động thái nào chuyển nước đi trước.
+        return outcome(m); //Quay lại trực tiếp kết quả cuối cùng
+      othe.pass(m); //Thực thi bỏ qua
+      return -alphaBeta(m, depth, -beta, -alpha);
+    }
+
+    if (depth <= 0) {
+      //Độ sâu tìm kiếm đạt đến giới hạn đã đặt
+      var e = evaluation(m);
+      hash.set(m.key, e, depth, 0, null); // Đặt phương pháp thay thế bảng
+      return e; //Trả lại giá trị đánh giá khi nó đạt đến giá trị sâu nhất
+    }
+
+    var hd = hash.getBest(m.key); //Những gì được cho làphashe.best
+    if (hd !== null) moveToHead(m.nextIndex, hd); //Đến nút trên cùng
+
+    var hist = _this.history[m.side == 1 ? 0 : 1][m.space]; //Bảng heuristic lịch sử được chỉ định cho history
+    var hashf = 1; //Loại định giá tốt nhất, 0 là giá trị chính xác, 1 là <= alpha, 2 là> = beta
+    var bestVal = -Infinity; //Ghi lại mức định giá tốt nhất
+    var bestAct = null; //Ghi lại động thái tốt nhất
+    for (var i = 0; i < m.nextNum; i++) {
+      //Liên tục tìm kiếm cắt tỉa cho lớp tiếp theo
+      var n = m.nextIndex[i];
+      var v = -alphaBeta(othe.newMap(m, n), depth - 1, -beta, -alpha);
+      if (v > bestVal) {
+        bestVal = v;
+        bestAct = n;
+        if (v > alpha) {
+          alpha = v;
+          hashf = 0; //Cung cấp giá trị chính xác
+          moveToUp(hist, n); //Quay lại nút trên
+        }
+        if (v >= beta) {
+          hashf = 2;
+          break; //Cắt tỉa xảy ra
+        }
+      }
+    }
+    moveToHead(hist, bestAct); //Quay lại nút trên cùng
+    hash.set(m.key, bestVal, depth, hashf, bestAct); //Phương thức thiết lập của bảng thay thế: bestAct được trả về từ đây
+    return bestVal; // giống với giá trị cuối cùng của đánh giá trên
+  }
+
+  function moveToHead(arr, n) {
+    //alphabeta Được sử dụng để quay lại cấp cao nhất của cây
+    if (arr[0] == n) return;
+    var i = arr.indexOf(n);
+    arr.splice(i, 1);
+    arr.unshift(n);
+  }
+
+  function moveToUp(arr, n) {
+    //alphabeta Nó được sử dụng để quay lại nút trên
+    if (arr[0] == n) return;
+    var i = arr.indexOf(n);
+    arr[i] = arr[i - 1];
+    arr[i - 1] = n;
+  }
 }
 
+function Transposition() {
+  //Bảng hoán vị
 
-function Transposition(){//置换表
-	
-	var oo = this;
+  var _this = this;
 
-	var HASH_SIZE = (1 << 19) -1;		//置换单元数为 524287
-	var data = new Array(HASH_SIZE+1);
+  var HASH_SIZE = (1 << 19) - 1; //Số đơn vị thay thế là 524287
+  var data = new Array(HASH_SIZE + 1);
 
-	oo.set = function (key,eva,depth,flags,best){
-		
-		var keyb = key[0]&HASH_SIZE;
-		var phashe = data[keyb];
-		if (!phashe)
-			phashe = data[keyb] = {};
-		else if (phashe.key == key[1] && phashe.depth > depth)		//局面相同 并且 记录比当前更深 则不替换
-			return;
-		phashe.key = key[1];
-		phashe.eva = eva;
-		phashe.depth = depth;
-		phashe.flags = flags;
-		phashe.best = best;
-	}
+  _this.set = function (key, eva, depth, flags, best) {
+    var keyb = key[0] & HASH_SIZE;
+    var phashe = data[keyb];
+    if (!phashe) phashe = data[keyb] = {};
+    else if (phashe.key == key[1] && phashe.depth > depth)
+      //Nếu bản ghi sâu hơn bản ghi hiện tại,
+      //		nó sẽ không được thay thế
 
-	oo.get = function (key,depth,alpha,beta){
-		var phashe = data[key[0]&HASH_SIZE];
-		if ((!phashe) || phashe.key != key[1] || phashe.depth < depth)
-			return false;
-		switch (phashe.flags){
-			case 0:
-				return phashe.eva;
-			case 1:
-				if (phashe.eva <= alpha)
-					return phashe.eva;
-				return false;
-			case 2:
-				if (phashe.eva >= beta)
-					return phashe.eva;
-				return false;
-		}
-	}
+      return;
+    phashe.key = key[1];
+    phashe.eva = eva;
+    phashe.depth = depth;
+    phashe.flags = flags;
+    phashe.best = best;
+  };
 
-	oo.getBest = function (key){
-		var phashe = data[key[0]&HASH_SIZE];
-		if ((!phashe) || phashe.key != key[1])
-			return null;
-		return phashe.best;
-	}
+  _this.get = function (key, depth, alpha, beta) {
+    var phashe = data[key[0] & HASH_SIZE];
+    if (!phashe || phashe.key != key[1] || phashe.depth < depth) return false;
+    switch (phashe.flags) {
+      case 0:
+        return phashe.eva;
+      case 1:
+        if (phashe.eva <= alpha) return phashe.eva;
+        return false;
+      case 2:
+        if (phashe.eva >= beta) return phashe.eva;
+        return false;
+    }
+  };
+
+  _this.getBest = function (key) {
+    var phashe = data[key[0] & HASH_SIZE];
+    if (!phashe || phashe.key != key[1]) return null;
+    return phashe.best;
+  };
 }
-
